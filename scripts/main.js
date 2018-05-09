a4c9d1cee1748713937d25694d62152f07f3429b
EOussama <oussama_gtacj@live.fr>
2018-05-09 16:12:36 +0100

    Why wont it work?

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,160 @@
-console.log('Hello, world!');
+var Card = /** @class */ (function () {
+    function Card(hiragana, english, card) {
+        this._card = card;
+        this._hiragana = hiragana;
+        this._english = english;
+    }
+    Card.prototype.Flip = function () {
+        if (!this._card.classList.contains('flipped'))
+            this._card.classList.add('flipped');
+        else
+            this._card.classList.remove('flipped');
+        console.log(this._card);
+    };
+    return Card;
+}());
+var Deck = /** @class */ (function () {
+    function Deck(cards) {
+        this._cards = cards;
+    }
+    Deck.prototype.Display = function () {
+        var _loop_1 = function (card) {
+            var charCard = document.createElement('div'), charCardFront = document.createElement('div'), charCardBack = document.createElement('div');
+            charCard.classList.add('card');
+            charCardFront.classList.add('face');
+            charCardFront.classList.add('front');
+            charCardFront.textContent = card._hiragana;
+            if (card._hiragana.length > 1)
+                charCardFront.style.fontSize = '80px';
+            charCardBack.classList.add('face');
+            charCardBack.classList.add('back');
+            charCardBack.textContent = card._english;
+            if (card._english.length > 1)
+                charCardBack.style.fontSize = '40px';
+            charCard.appendChild(charCardFront);
+            charCard.appendChild(charCardBack);
+            charsContainer.appendChild(charCard);
+            charCard.addEventListener('click', function () {
+                card._card = charCard;
+                card.Flip();
+            });
+        };
+        for (var _i = 0, _a = this._cards; _i < _a.length; _i++) {
+            var card = _a[_i];
+            _loop_1(card);
+        }
+    };
+    return Deck;
+}());
+var deck = new Deck([
+    new Card('あ', 'a'),
+    new Card('い', 'i'),
+    new Card('う', 'u'),
+    new Card('え', 'e'),
+    new Card('お', 'o'),
+    new Card('か', 'ka'),
+    new Card('き', 'ki'),
+    new Card('く', 'ku'),
+    new Card('け', 'ke'),
+    new Card('こ', 'ko'),
+    new Card('きゃ', 'kya'),
+    new Card('きゅ', 'kyu'),
+    new Card('きょ', 'kyo'),
+    new Card('さ', 'sa'),
+    new Card('し', 'shi'),
+    new Card('す', 'su'),
+    new Card('せ', 'se'),
+    new Card('そ', 'so'),
+    new Card('しゃ', 'sha'),
+    new Card('しゅ', 'shu'),
+    new Card('しょ', 'sho'),
+    new Card('た', 'ta'),
+    new Card('ち', 'chi'),
+    new Card('つ', 'tsu'),
+    new Card('て', 'te'),
+    new Card('と', 'to'),
+    new Card('ちゃ', 'cha'),
+    new Card('ちゅ', 'chu'),
+    new Card('ちょ', 'cho'),
+    new Card('な', 'na'),
+    new Card('に', 'ni'),
+    new Card('ぬ', 'nu'),
+    new Card('ね', 'ne'),
+    new Card('の', 'no'),
+    new Card('にゃ', 'nya'),
+    new Card('にゅ', 'nyu'),
+    new Card('にょ', 'nyo'),
+    new Card('は', 'ha'),
+    new Card('ひ', 'hi'),
+    new Card('ふ', 'hu'),
+    new Card('へ', 'he'),
+    new Card('ほ', 'ho'),
+    new Card('ひゃ', 'hya'),
+    new Card('ひゅ', 'hyu'),
+    new Card('ひょ', 'hyo'),
+    new Card('ま', 'ma'),
+    new Card('み', 'mi'),
+    new Card('む', 'mu'),
+    new Card('め', 'me'),
+    new Card('も', 'mo'),
+    new Card('みゃ', 'mya'),
+    new Card('みゅ', 'myu'),
+    new Card('みょ', 'myo'),
+    new Card('や', 'ya'),
+    new Card('ゆ', 'yu'),
+    new Card('よ', 'yo'),
+    new Card('ら', 'ra'),
+    new Card('り', 'ri'),
+    new Card('る', 'ru'),
+    new Card('れ', 're'),
+    new Card('ろ', 'ro'),
+    new Card('りゃ', 'rya'),
+    new Card('りゅ', 'ryu'),
+    new Card('りょ', 'ryo'),
+    new Card('わ', 'wa'),
+    new Card('ゐ', 'wi'),
+    new Card('ゑ', 'we'),
+    new Card('を', 'wo'),
+    new Card('ん', 'n'),
+    new Card('が', 'ga'),
+    new Card('ぎ', 'gi'),
+    new Card('ぐ', 'gu'),
+    new Card('げ', 'ge'),
+    new Card('ご', 'go'),
+    new Card('ぎゃ', 'gya'),
+    new Card('ぎゅ', 'gyu'),
+    new Card('ぎょ', 'gyo'),
+    new Card('ざ', 'za'),
+    new Card('じ', 'ji'),
+    new Card('ず', 'zu'),
+    new Card('ぜ', 'ze'),
+    new Card('ぞ', 'zo'),
+    new Card('じゃ', 'ja'),
+    new Card('じゅ', 'ju'),
+    new Card('じょ', 'jo'),
+    new Card('だ', 'da'),
+    new Card('ぢ', 'ji'),
+    new Card('づ', 'zu'),
+    new Card('で', 'de'),
+    new Card('ど', 'do'),
+    new Card('ぢゃ', 'ja'),
+    new Card('ぢゅ', 'ju'),
+    new Card('ぢょ', 'jo'),
+    new Card('ば', 'ba'),
+    new Card('び', 'bi'),
+    new Card('ぶ', 'bu'),
+    new Card('べ', 'be'),
+    new Card('ぼ', 'bo'),
+    new Card('びゃ', 'bya'),
+    new Card('びゅ', 'byu'),
+    new Card('びょ', 'byo'),
+    new Card('ぱ', 'pa'),
+    new Card('ぴ', 'pi'),
+    new Card('ぷ', 'pu'),
+    new Card('ぺ', 'pe'),
+    new Card('ぽ', 'po'),
+    new Card('ぴゃ', 'pya'),
+    new Card('ぴゅ', 'pyu'),
+    new Card('ぴょ', 'pyo')
+]), charsContainer = document.getElementById('charactersContainer');
+deck.Display();
